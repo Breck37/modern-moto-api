9ebf32dcd17b0bbc9f4a00f4b7d8c7424fc15a18
B-eck37 <brent.eckert7@gmail.com>
2021-02-12 21:06:51 -0700

    chore: first commit

---
+++ api/get-user.js
@@ -0,0 +1,43 @@
+// Import Dependencies
+const MongoClient = require('mongodb').MongoClient
+
+// Create cached connection variable
+let cachedDb = null
+
+// A function for connecting to MongoDB,
+// taking a single parameter of the connection string
+async function connectToDatabase(uri) {
+  // If the database connection is cached,
+  // use it instead of creating a new connection
+  if (cachedDb) {
+    return cachedDb
+  }
+
+  // If no connection is cached, create a new one
+  const client = await MongoClient.connect(uri, { useNewUrlParser: true })
+
+  const url = new URL(uri).athname.substr(1);
+
+  // Select the database through the connection,
+  // using the database path of the connection string
+  const db = await client.db(url)
+
+  // Cache the database connection and return the connection
+  cachedDb = db
+  return db
+}
+
+module.exports = async (req, res) => {
+    const { email } = req.params;
+
+    const db = await connectToDatabase(process.env.LOCAL_MONGO_URI);
+
+    const user = await db.collection('users').findOne({ email });
+
+    if (!user || (Array.isArray(user) && !user.length)) {
+      const result = await createUser({ email });
+      return res.status(200).json(result);
+    }
+
+    return res.status(200).json({ success: true, user });
+  };
