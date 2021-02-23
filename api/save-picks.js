2f995b0f3385be6b9ea094b31938deec1c52e3c6
B-eck37 <brent.eckert7@gmail.com>
2021-02-22 19:56:29 -0700

    refactor: improved save-picks
    Signed-off-by: B-eck37 <brent.eckert7@gmail.com>

--- api/save-picks.js
+++ api/save-picks.js
@@ -2,18 +2,18 @@ import connectToDatabase from './utils/connectToDatabase';
 
 module.exports = async (req, res) => {
     console.log('HIHIHIT')
-    const { email, picks, week } = req.body;
-    console.log(picks, req.body, req.query)
+    const { email, bigBikePicks, week } = req.body;
+    console.log({ picks: bigBikePicks, Body: req.body, query: req.query})
     const db = await connectToDatabase(process.env.MONGO_URI);
 
     const formattedUserPicks = {
         email,
         week,
-        picks,
+        bigBikePicks,
         totalPoints: 0
     }
 
     await db.collection('picks').insertOne(formattedUserPicks)
 
-    res.status(200).json({ success: true, picks, email, week })
+    res.status(200).json({ success: true, bigBikePicks, email, week })
 }
