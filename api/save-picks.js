0703c64533575ca2c7285ca3748e16ddae7da8f3
B-eck37 <brent.eckert7@gmail.com>
2021-03-03 08:48:12 -0700

    fix: remove erroneous log
    Signed-off-by: B-eck37 <brent.eckert7@gmail.com>

--- api/save-picks.js
+++ api/save-picks.js
@@ -1,7 +1,6 @@
 import connectToDatabase from './utils/connectToDatabase';
 
 module.exports = async (req, res) => {
-    console.log('HIHIHIT')
     const { email, bigBikePicks, week } = req.body;
     console.log({ picks: bigBikePicks, Body: req.body, query: req.query})
     const db = await connectToDatabase(process.env.MONGO_URI);
