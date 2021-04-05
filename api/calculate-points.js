7c6b928c0a7aeff2e7229bb1639b743d0f715779
Breck37 <brent.eckert7@gmail.com>
2021-04-04 19:31:40 -0700

    refactor: calculate-points wip
    Signed-off-by: Breck37 <brent.eckert7@gmail.com>

--- api/calculate-points.js
+++ api/calculate-points.js
@@ -1,5 +1,13 @@
 import connectToDatabase from './utils/connectToDatabase';
 
 module.exports = async (req, res) => {
-    res.status(200).json({ success: true });
+    const { week, raceResults } = req.query;
+
+    const db = await connectToDatabase(process.env.MONGO_URI);
+
+    const currentWeekPicks = await db.collection('picks').find({ week }).toArray();
+
+    console.log({ currentWeekPicks });
+
+    res.status(200).json({ success: true, currentWeekPicks });
 }
