5aae4db94abfc664954588c7468bd5b306ff0f01
Breck37 <brent.eckert7@gmail.com>
2021-04-13 14:35:13 -0700

    fix: log picks
    Signed-off-by: Breck37 <brent.eckert7@gmail.com>

--- api/save-picks.js
+++ api/save-picks.js
@@ -1,24 +1,30 @@
-import connectToDatabase from './utils/connectToDatabase';
+import connectToDatabase from "./utils/connectToDatabase";
 
 module.exports = async (req, res) => {
-    const { email, bigBikePicks, smallBikePicks = [], week, league = "" } = req.body;
+  const {
+    email,
+    bigBikePicks,
+    smallBikePicks = [],
+    week,
+    league = "",
+  } = req.body;
 
-    const db = await connectToDatabase(process.env.MONGO_URI);
+  const db = await connectToDatabase(process.env.MONGO_URI);
 
-    const formattedUserPicks = {
-        user: email,
-        week,
-        year: new Date().getFullYear(),
-        bigBikePicks,
-        smallBikePicks,
-        totalPoints: 0,
-        hasBeenEquated: false,
-        league,
-        rank: null,
-        created_at: new Date()
-    }
+  const formattedUserPicks = {
+    user: email,
+    week,
+    year: new Date().getFullYear(),
+    bigBikePicks,
+    smallBikePicks,
+    totalPoints: 0,
+    hasBeenEquated: false,
+    league,
+    rank: null,
+    created_at: new Date(),
+  };
+  console.log({ formattedUserPicks });
+  await db.collection("picks").insertOne(formattedUserPicks);
 
-    await db.collection('picks').insertOne(formattedUserPicks)
-
-    res.status(200).json({ success: true, bigBikePicks, email, week })
-}
+  res.status(200).json({ success: true, bigBikePicks, email, week });
+};
