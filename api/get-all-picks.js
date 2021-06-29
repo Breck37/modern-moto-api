e2c6fc1568a52c7575a363ed2e1c5f096ee77bcd
Brent Eckert <brent.eckert7@gmail.com>
2021-06-28 22:04:16 -0700

    refactor: adding optional type to get-all-picks
    Signed-off-by: Brent Eckert <brent.eckert7@gmail.com>

--- api/get-all-picks.js
+++ api/get-all-picks.js
@@ -1,9 +1,15 @@
 import connectToDatabase from "./utils/connectToDatabase";
 
 module.exports = async (req, res) => {
+    const { query = {} } = req;
+    let pickQuery = {};
     const db = await connectToDatabase(process.env.MONGO_URI);
 
-    const picks = await db.collection('picks').find().toArray();
+    if (query && query.type) {
+        pickQuery.type = query.type;
+    }
+
+    const picks = await db.collection('picks').find(pickQuery).toArray();
 
     res.status(200).send(picks)
-} 
+}
