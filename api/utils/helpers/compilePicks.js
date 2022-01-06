4a4f9133f98fb40207b9498fa77afebd2aa81cbf
Brent Eckert <brent.eckert7@gmail.com>
2022-01-05 22:29:59 -0700

    chore: remove logs

--- api/utils/helpers/compilePicks.js
+++ api/utils/helpers/compilePicks.js
@@ -2,7 +2,6 @@ const { decipher } = require('./decipher');
 
 export const compileLeaguePicks = (leaguePicks, currentPicks) => {
   let picks = leaguePicks ?? {};
-  console.log({ currentPicks })
   if (Array.isArray(currentPicks)) {
     currentPicks.map((currentPick) => {
       if (Array.isArray(currentPick)) {
